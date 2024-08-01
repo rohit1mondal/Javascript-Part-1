03ecb96a21e44fc7ba4a23fae8699293a85186b9
Rohit Mondal <105375601+Rohit12m@users.noreply.github.com>
2024-08-01 12:52:20 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,27 @@
+let score = "33abx" //1st value = 33 then 33abx then null
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "33"= 33
+// "33abx"= NaN
+//  true =>1 , false =>0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1=>true , 0=> false
+// "" =>false
+// "Rohit" => true
+
+let someNumber = 33
+
+let stingNumber = String(someNumber)
+console.log(stingNumber);
+console.log(typeof stingNumber);
