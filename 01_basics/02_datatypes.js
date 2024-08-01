bf9c01e1cc7efbf12b5bc2f863c0bf7070d51300
Rohit Mondal <105375601+Rohit12m@users.noreply.github.com>
2024-08-01 12:02:44 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version!
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(6+6);console.log("Rohit"); // code readability should be high
+
+let name = "Rohit"
+let age = 22
+let isLoggedin = false
+let state;
+
+// number => 2 to power 53
+// bigint = 
+// string =>""
+// boolean =>true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined 
