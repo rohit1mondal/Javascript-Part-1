2bd30d7c166a893346425b122a894e5636dc6fd1
Rohit Mondal <105375601+Rohit12m@users.noreply.github.com>
2024-08-02 07:07:35 +0000

    discuss on operations

--- 01_basics/04_operations.js
+++ 01_basics/04_operations.js
@@ -25,4 +25,11 @@ let str3 = str1 + str2
 // console.log(+true);
 // console.log(+""); // avoid to write confusing codes
 
+let num1,num2,num3
+
+num1 =num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
 
