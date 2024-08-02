371270e90c64f3648b5d6ae0fb5201fa580a808a
Rohit Mondal <105375601+Rohit12m@users.noreply.github.com>
2024-08-02 05:03:20 +0000

    discussion on operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,28 @@
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " Rohit"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+ 2);
+// console.log(2+ "1");
+// console.log(1 + 2+ "2");
+// console.log("2" + 2 +1); //exam mindset questions
+
+// console.log((2+3) * 88 /7); // code should be clean ,readable,simple
+
+// console.log(true);
+// console.log(+true);
+// console.log(+""); // avoid to write confusing codes
+
+
