0e4ee2b6e9caad49449f5b2b24055e9a3c656f4b
Rohit Mondal <105375601+Rohit12m@users.noreply.github.com>
2024-08-03 13:43:51 +0000

    Dates and time

--- 01_basics/10_dates_and_time.js
+++ 01_basics/10_dates_and_time.js
@@ -3,8 +3,8 @@
 let mydate = new Date()
 
 console.log(mydate.toString());
-console.log(mydate.toDateString);
-console.log(mydate.toLocaleString);
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
 console.log(typeof mydate);
 
 // let myCreatedDate = new Date(2024, 5, 30)
