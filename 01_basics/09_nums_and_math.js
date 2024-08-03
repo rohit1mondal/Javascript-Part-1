e63c165a61b615aa2372758d43b9b15717ba4f65
Rohit Mondal <105375601+Rohit12m@users.noreply.github.com>
2024-08-03 11:58:19 +0000

    nums and math

---
+++ 01_basics/09_nums_and_math.js
@@ -0,0 +1,40 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber = 123.4566
+
+// console.log(otherNumber.toPrecision(3));
+
+// const nums = 10000000
+// console.log(nums.toLocaleString('en-IN'));
+
+
+// //+++++++++++++++++++++++ Math +++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-84));
+console.log(Math.round(8.5));
+console.log(Math.ceil(8.5));
+console.log(Math.floor(8.5));
+console.log(Math.min(8, 5, 9, 2));
+console.log(Math.max(8, 5, 6, 7));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
