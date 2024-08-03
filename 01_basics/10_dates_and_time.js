019c6cf6103d02d7a7fd444d5009fe9fdbcdd8ef
Rohit Mondal <105375601+Rohit12m@users.noreply.github.com>
2024-08-03 13:32:55 +0000

    Dates and time

---
+++ 01_basics/10_dates_and_time.js
@@ -0,0 +1,33 @@
+//Dates
+
+let mydate = new Date()
+
+console.log(mydate.toString());
+console.log(mydate.toDateString);
+console.log(mydate.toLocaleString);
+console.log(typeof mydate);
+
+// let myCreatedDate = new Date(2024, 5, 30)
+let myCreatedDate = new Date(2024, 5, 30, 7, 6)
+// let myCreatedDate = new Date("2024-05-30")
+// let myCreatedDate = new Date("05-30-2024")
+console.log(myCreatedDate.toLocaleString());
+console.log(myCreatedDate.toDateString());
+
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDay());
+console.log(newDate.getHours());
+
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    // timeZone: 45
+})
