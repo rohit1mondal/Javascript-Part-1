465e2d834bc268d3236c70798fa7e01e4b21c22d
Rohit Mondal <105375601+Rohit12m@users.noreply.github.com>
2024-08-03 03:55:12 +0000

    discuss on memory

---
+++ memory.js
@@ -0,0 +1,22 @@
+// stack (primitive), Heap(Non-primitive)
+//Stack = change in a copy value || Heap = change in the reference
+
+let name = "Rohit"
+
+let anothername = name
+anothername = "hey"
+
+console.log(name);
+console.log(anothername);
+ 
+let userOne = {
+    email: "Roh@gamil.com",
+    upi : "jhy@ybl"
+}
+let userTwo  = userOne
+
+userTwo.email = "toh@gamil.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
