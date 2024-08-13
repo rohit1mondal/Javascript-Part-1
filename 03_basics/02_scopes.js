63a410cf60c1b488e3c420c3ba8db426412240be
Rohit Mondal <105375601+rohit1mondal@users.noreply.github.com>
2024-08-13 08:56:32 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -13,3 +13,45 @@ if (true) {
 console.log(a);
 console.log(b);
 console.log(c);
+
+//scope level and mini hoisting
+
+function one() {
+    const username = "rohit"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    console.log(website);
+    
+    two()
+}
+one()
+
+
+if (true) {
+    const username = "rohit"
+    if (username === "rohit") {
+        const website = "youtube"
+        console.log(username + website);
+    }
+    console.log(website);
+}
+
+console.log(username);
+
+
+//++++++++++++++++++++++  Interesting  ++++++++++++++++++++
+
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+
+
+addtwo(5)
+const addtwo = function(num){
+    return num + 2
+}
