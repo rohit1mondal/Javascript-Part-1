81c8b5b532501392c3ebf959b99fc04f6e91925a
Rohit Mondal <105375601+rohit1mondal@users.noreply.github.com>
2024-09-07 18:37:44 +0000

    iteration

--- 05_iteration /loops.js
+++ 05_iteration /loops.js
@@ -22,11 +22,28 @@ for (let index = 0; index < array.length; index++) {
 //     }
 // }
 
-let myArray = ["superman", "batman", "flash"]
-console.log(myArray.length);
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    console.log(element);
-    
-}
+// let myArray = ["superman", "batman", "flash"]
+// console.log(myArray.length);
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element);
+// }
 
+// break and continue
+
+// for (let index = 0; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         break
+//     }
+//     console.log(`Value of i is ${index}`);
+// }
+
+
+// for (let index = 0; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         continue
+//     }
+//     console.log(`Value of i is ${index}`);
+// }
