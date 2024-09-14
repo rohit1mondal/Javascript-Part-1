fcb8b605ec8095496f01c13c378502de3ab3569b
Rohit Mondal <105375601+rohit1mondal@users.noreply.github.com>
2024-09-14 14:47:14 +0000

    for loop

--- 05_iteration/loop.js
+++ 05_iteration/loop.js
@@ -1,17 +1,17 @@
 // for
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-}
-
-// for (let i = 0; i <= 10; i++) {
-//     const element = i;
-//     if (element == 5) {
-//         console.log("5 is the best number");
-//     }
-//     console.log(element);    
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
 // }
-// console.log(element);
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        console.log("5 is the best number");
+    }
+    console.log(element);    
+}
+console.log(element);
 
 // for (let i = 0; i <= 10; i++) {
 //     console.log(`Outer loop value: ${i}`);
