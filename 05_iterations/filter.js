e7c04b110624dca618f45b7901114a0ddb9cbfd1
Rohit Mondal <105375601+rohit1mondal@users.noreply.github.com>
2024-09-21 13:57:02 +0000

    filter

--- 05_iterations/filter.js
+++ 05_iterations/filter.js
@@ -1,8 +1,31 @@
 const coding = ["js", "python", "java", "c", "cpp"]
 
-const values = coding.forEach( (item) => {
-    console.log(item);
-    return item
-})
+// const values = coding.forEach( (item) => {
+//     console.log(item);
+//     return item
+// })
 
-console.log(values);
+// console.log(values);
+
+//filter method:
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+
+// const newNums = myNums.filter( (num) => num > 4)
+
+// const newNums = myNums.filter( (num) => {
+//     return num > 4
+// })
+
+// for each method
+const newNums = []
+
+// myNums.forEach( (num) => {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// })
+
+// console.log(newNums);
+
+
